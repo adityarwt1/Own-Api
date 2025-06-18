5d9ac2270cf78c1d33697c93fefc5904f2094bb0
adityarwt1 <adityarawatnew2487@gmail.com>
2025-06-18 09:35:30 +0530

    api dleted

---
+++ app/api/custom/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    /// your db action
+    return NextResponse.json(
+      {
+        title: "This is the title",
+        description: "This is the description",
+        provider: "Aditya Rawat",
+      },
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST , PUT, DELETE",
+          "Access-Control-Allow-Headers": "Content-Type , Authorization",
+        },
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Issue" },
+      { status: 500 }
+    );
+  }
+}
